7d9fbc3f5e950af15a9202ea3f91e39f32beadc4
bmcconnan <brockmcconnan@gmail.com>
2026-08-22 14:37:26 -0700

    Clarify calculator model settings

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "pilot-rest-v22";
+const CACHE_NAME = "pilot-rest-v23";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./pilot-rest-v1-break-overrides-styles.css?v=20260822-8",
+  "./pilot-rest-v1-break-overrides-styles.css?v=20260822-9",
   "./pilot-rest-v1-break-overrides-app.js?v=20260822-10",
   "./manifest.webmanifest",
   "./icon-192.png",
